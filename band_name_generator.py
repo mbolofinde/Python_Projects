2817d9863199131380657ddcb0688dfcc086f66b
Matthew Bolofinde <molanrewajuking@gmail.com>
2024-01-23 13:39:15 -0600

    revised output line

--- band_name_generator.py
+++ band_name_generator.py
@@ -6,7 +6,13 @@ print("Welcome to Band Generator")
 
 # Ask user for thier name
 #
-name = input("Enter your firstname: ") 
+name = input("Enter your firstname:") 
 
 print(f"\n Welcome {name} to Band Generator") 
  
+# Ask user for thier location
+
+location = input("Enter your location: ")
+print(f'\n Your Bandname is {name}_{location}') 
+
+
